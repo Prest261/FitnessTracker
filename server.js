37acc89368154107d8a7b4b58d563fb3b185c330
Syd Rigel <prest261@umn.edu>
2020-04-08 21:59:57 -0600

    fixed the mime error, the GET error, and all the problems stemming from the Develop folder

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // require the routes
 
-app.use(require("./Develop/routes/api"));
-app.use(require("./Develop/routes/html"));
+app.use(require("./routes/api"));
+app.use(require("./routes/html"));
 // Start the server
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
